1967a343d350306a2e8c570ee08e6a9afc7c3855
Josh-techie <youse2002@outlook.fr>
2025-05-09 23:38:50 +0100

    Feat:Setup-First-Page-Mehdoune

---
+++ education-platform-mvp/src/app/components/HomePage.tsx
@@ -0,0 +1,17 @@
+export default function HomePage() {
+  return (
+    <div className="home-page">
+      <h1 className="text-4xl font-bold text-center mt-8 bg-sky-300">Welcome to the Language Center</h1>
+      <p className="text-center mt-4 text-gray-600">
+        Explore our educational platform and enhance your language skills.
+      </p>
+
+      <div className="flex space-x-4">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+        </div>
+
+    </div>
+  );
+}
